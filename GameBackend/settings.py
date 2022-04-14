db63088e48c5ea1cf872259154bf4a846181e8b6
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2022-04-14 16:26:31 +0200

    added database connectuin

--- GameBackend/settings.py
+++ GameBackend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os, sys
 from pathlib import Path
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -99,6 +99,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
